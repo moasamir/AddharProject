2646bb7db387e1ae45ce0d395990dfc18bd3e308
Mohammad Samir <mdsamir977366@gmail.com>
2026-02-01 20:55:18 +0530

    deploy flutter web via docs folder

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d789f71c4a067245e1ee398b146c837d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0959b449674799cbf396b4390bd992d1",
+"flutter_bootstrap.js": "5c9f2eb164324eabf733edeeff8f70c0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
